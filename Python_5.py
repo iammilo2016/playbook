334654eccfef2b4451a327bc90796b441a1c9e35
iammilo2016 <alanmiloto@gmail.com>
2024-01-02 23:49:58 -0700

    Add files via upload

---
+++ Python_5.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def run_ansible_playbook(playbook_path):
+    try:
+        # Running the Ansible playbook
+        subprocess.run(["ansible-playbook", "-i", "/home/waiwa/Assignment_5/inventory", playbook_path], check=True)
+        print("Playbook executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    playbook_path = "/home/waiwa/Assignment_5/Playbook_1.yml"
+    run_ansible_playbook(playbook_path)
