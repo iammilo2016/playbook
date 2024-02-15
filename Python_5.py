ffe3d26f616a2714fee7ae9d3b1fc2ec07f91f19
iammilo2016 <alanmiloto@gmail.com>
2024-02-15 14:22:56 -0700

    Update Python_5.py

--- Python_5.py
+++ Python_5.py
@@ -3,11 +3,11 @@ import subprocess
 def run_ansible_playbook(playbook_path):
     try:
         # Running the Ansible playbook
-        subprocess.run(["ansible-playbook", "-i", "/home/waiwa/Assignment_5/inventory", playbook_path], check=True)
+        subprocess.run(["ansible-playbook", "-i", "/home/user/Assignment_5/inventory", playbook_path], check=True)
         print("Playbook executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    playbook_path = "/home/waiwa/Assignment_5/Playbook_1.yml"
+    playbook_path = "/home/user/Assignment_5/Playbook_1.yml"
     run_ansible_playbook(playbook_path)
